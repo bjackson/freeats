7d0102e9144f76d3534669115f9460ebcdfd8676
Brett Jackson <brettbj.jackson@gmail.com>
2012-09-07 19:49:27 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Freeats::Application.config.secret_token = '653fbdd650767ee32cecc2a345b18e4bd04d747be269bbc7f49eacc5bc8f1e4db6abbbe69606eba2256211c0f81ad0738bf1be04e49e082a29c6f42177e6d44a'
