53ecd648c707b920e6946ab2549f13442389633e
Brett Jackson <brettbj.jackson@gmail.com>
2012-09-08 13:10:52 -0500

    implemented map on show page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -19,6 +19,7 @@ class EventsController < ApplicationController
   # GET /events/1.json
   def show
     @event = Event.find(params[:id])
+    @event_json = @event.to_gmaps4rails
 
     respond_to do |format|
       format.html # show.html.erb
